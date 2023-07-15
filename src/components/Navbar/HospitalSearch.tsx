cb3620dde2a0e996a76275f75929226a4c7e996c
Adezb <temitopeceo@gmail.com>
2023-07-15 07:21:22 +0100

    feature update

--- src/components/Navbar/HospitalSearch.tsx
+++ src/components/Navbar/HospitalSearch.tsx
@@ -2,21 +2,34 @@
 import { useState, ChangeEvent } from "react";
 import HospitalEntryForm from "./Modals/HospitalEntryForm";
 import About from "./About/About";
+import { CSVLink } from "react-csv";
+
+import {
+  WhatsappShareButton,
+  FacebookShareButton,
+  TwitterShareButton,
+  EmailShareButton,
+} from "react-share";
 import {
   BiShareAlt,
   BiExport,
   BiChevronLeft,
   BiChevronRight,
 } from "react-icons/bi";
+import { FaWhatsapp, FaFacebook, FaTwitter, FaSpinner } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import useFetchHospitals from "@/hook/useFetchHospitals";
 import Image from "next/image";
+import { auth } from "@/firebase/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type HospitalSearchProps = {};
 
 const HospitalSearch: React.FC<HospitalSearchProps> = () => {
   const [city, setCity] = useState("");
+  const [user] = useAuthState(auth);
   const {
     loading,
     message,
@@ -24,9 +37,34 @@ const HospitalSearch: React.FC<HospitalSearchProps> = () => {
     fetchMoreHospitals,
     fetchPreviousHospitals,
     handleSearchHospital,
-  } = useFetchHospitals();
+  } = useFetchHospitals(city);
+
+  const exportHospitals = () => {
+    const csvData = hospitals.map((hospital) => {
+      return [
+        hospital.name,
+        hospital.ownership,
+        hospital.address,
+        hospital.city,
+        hospital.email,
+        hospital.phone,
+      ];
+    });
+    const csvLink = new CSVLink({
+      data: csvData,
+      filename: "hospitals.csv",
+    });
+    return csvLink;
+  };
 
   const handleCityChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setCity(e.target.value);
+    handleSearchHospital();
+  };
+
+  const handleSelectInput = (e: ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
     setCity(e.target.value);
   };
 
@@ -47,28 +85,126 @@ const HospitalSearch: React.FC<HospitalSearchProps> = () => {
           >
             <HospitalEntryForm />
           </Popup>
-          <div className="mt-5">
+          {/* Input tag for hospital search and select tag for city option div */}
+          <div className="flex flex-col sm:flex-row sm:justify-center sm:items-center mt-5">
             <input
               type="text"
-              placeholder="Search hospitals by City"
-              className="border-2 border-gray-300 rounded-md p-2 focus:outline-none"
+              placeholder="Find Hospitals by City"
+              className="border-2 border-gray-300 rounded-md p-2 sm:mr-2 focus:outline-none"
               value={city}
               onChange={handleCityChange}
             />
-            <button
-              className="bg-btn-blue p-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
-              onClick={() => handleSearchHospital(city)}
+            <select
+              onChange={handleSelectInput}
+              name="city"
+              id="city"
+              className="border-2 border-gray-300 rounded-md p-2 mt-2 sm:mt-0 bg-gray-transparent-dark text-gray-light focus:outline-none"
             >
-              Search
-            </button>
+              {/* Cities in Nigeria */}
+              <option value="">Select City</option>
+              <option value="Abuja">Abuja</option>
+              <option value="Abia">Abia</option>
+              <option value="Adamawa">Adamawa</option>
+              <option value="Akwa Ibom">Akwa Ibom</option>
+              <option value="Anambra">Anambra</option>
+              <option value="Bauchi">Bauchi</option>
+              <option value="Bayelsa">Bayelsa</option>
+              <option value="Benue">Benue</option>
+              <option value="Borno">Borno</option>
+              <option value="Cross River">Cross River</option>
+              <option value="Delta">Delta</option>
+              <option value="Ebonyi">Ebonyi</option>
+              <option value="Edo">Edo</option>
+              <option value="Ekiti">Ekiti</option>
+              <option value="Enugu">Enugu</option>
+              <option value="Gombe">Gombe</option>
+              <option value="Imo">Imo</option>
+              <option value="Jigawa">Jigawa</option>
+              <option value="Kaduna">Kaduna</option>
+              <option value="Kano">Kano</option>
+              <option value="Katsina">Katsina</option>
+              <option value="Kebbi">Kebbi</option>
+              <option value="Kogi">Kogi</option>
+              <option value="Kwara">Kwara</option>
+              <option value="Lagos">Lagos</option>
+              <option value="Nasarawa">Nasarawa</option>
+              <option value="Niger">Niger</option>
+              <option value="Ogun">Ogun</option>
+              <option value="Ondo">Ondo</option>
+              <option value="Osun">Osun</option>
+              <option value="Oyo">Oyo</option>
+              <option value="Plateau">Plateau</option>
+              <option value="Rivers">Rivers</option>
+              <option value="Sokoto">Sokoto</option>
+              <option value="Taraba">Taraba</option>
+              <option value="Yobe">Yobe</option>
+              <option value="Zamfara">Zamfara</option>
+            </select>
           </div>
-          {message ? (
+
+          {/* {message ? (
             <p className="border-2 border-gray-300 rounded-md p-2 mt-2 bg-gray-transparent-dark text-gray-light">
               {message}
             </p>
-          ) : null}
+          ) : null} */}
           <div className="mt-5 flex items-center justify-center">
             <div className="flex flex-col">
+              {/* User Dashboard with share button, export hospitals button and add hospital button */}
+              <>
+                {user ? (
+                  <div className="flex flex-row justify-center gap-5">
+                    {/* Share buttons using share icon and react-share */}
+
+                    <div className="flex flex-row gap-3 bg-gray-transparent-light px-4 py-2 rounded-md ">
+                      <BiShareAlt size={30} />
+                      <WhatsappShareButton
+                        url="https://medcare-finder-app.vercel.app/"
+                        title="Medcare Finder App"
+                        className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
+                        content={`${hospitals}` + `found in ${city}`}
+                      >
+                        <FaWhatsapp size={18} color="green" />
+                      </WhatsappShareButton>
+
+                      <FacebookShareButton
+                        url="https://medcare-finder-app.vercel.app/"
+                        quote="Medcare Finder App"
+                        className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
+                      >
+                        <FaFacebook size={18} color="blue" />
+                      </FacebookShareButton>
+
+                      <TwitterShareButton
+                        url="https://medcare-finder-app.vercel.app/"
+                        title="Medcare Finder App"
+                        className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
+                        content={`${hospitals}` + `found in ${city}`}
+                      >
+                        <FaTwitter size={18} color="skyblue" />
+                      </TwitterShareButton>
+
+                      <EmailShareButton
+                        url="https://medcare-finder-app.vercel.app/"
+                        subject="Medcare Finder App"
+                        className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
+                        content={`${hospitals}` + `found in ${city}`}
+                      >
+                        <AiOutlineMail size={18} />
+                      </EmailShareButton>
+                    </div>
+
+                    {/* Export button using react-csv */}
+                    <button className="bg-gray-transparent-light px-2 py-2 sm:px-4 rounded-md text-sm font-medium border-transparent">
+                      <CSVLink data={hospitals} filename={"hospitals.csv"}>
+                        <BiExport size={30} />
+                      </CSVLink>
+                    </button>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </>
+
               {hospitals.length > 0 && city !== " "
                 ? hospitals.map((hospital) => (
                     <div
@@ -109,41 +245,11 @@ const HospitalSearch: React.FC<HospitalSearchProps> = () => {
                           {hospital.phone}
                         </p>
                       </div>
-
-                      <div className="mt-6 flex gap-4 sm:gap-6">
-                        <div className="flex flex-col-reverse">
-                          <button className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out">
-                            <BiShareAlt />
-                          </button>
-                        </div>
-
-                        <div className="flex flex-col-reverse">
-                          <button className="bg-btn-blue px-1 py-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out">
-                            <BiExport />
-                          </button>
-                        </div>
-                      </div>
                     </div>
                   ))
                 : // Span message with border, background and text color
                   " "}
-              {/* Display Previous and Next button after showing list of hospitals */}
-              {/* {hospitals.length > 0 && (
-                <div className="mt-5 flex items-center justify-center">
-                  <button
-                    className="bg-btn-blue p-2 mr-5 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
-                    onClick={() => fetchPreviousHospitals}
-                  >
-                    <BiChevronLeft />
-                  </button>
-                  <button
-                    className="bg-btn-blue p-2 sm:px-4 rounded text-sm font-medium hover:text-btn-blue hover:bg-white hover:border-2 hover:border-btn-blue border-2 border-transparent transition duration-300 ease-in-out"
-                    onClick={() => fetchMoreHospitals}
-                  >
-                    <BiChevronRight />
-                  </button>
-                </div>
-              )} */}
+              {/* Fetch more hospital on scroll down */}
             </div>
           </div>
           <div className="mt-2">
--- src/hook/useFetchHospitals.ts
+++ src/hook/useFetchHospitals.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import {
     getDocs,
     collection,
@@ -26,26 +26,43 @@ import { db } from "@/firebase/firebase";
 
 
 
-function useFetchHospitals() {
+function useFetchHospitals( city: string) {
     const [loading, setLoading] = useState(true);
     const [message, setMessage] = useState<string | null>(null);
     const [hospitals, setHospitals] = useState<Hospital[]>([]);
-
-  
      
-    const perPage = 4;
+    const perPage = 5;
+
+    useEffect(() => {
+        const fetchHospital = async () => {
+            const q = query(collection(db, "hospitals"), where("city", "==", city), orderBy("name", "asc"));
+            try {
+                const querySnapshot = await getDocs(q);
+                const hospitals = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }))
+                setHospitals(hospitals as Hospital[]);
+                setLoading(false);
+                if (hospitals.length === 0) {
+                    setMessage(`No hospital data found for ${city}`);
+                    setTimeout(() => {
+                        setMessage("");
+                    }, 3000);
+                    return;
+                }
+            } catch (error: any) {
+                setMessage(error.message);
+            }
+        };
+        fetchHospital();
+    }, [city, perPage]  );
 
 
-    const handleSearchHospital = async (city: string) => {
-        if (city.trim() === "") {
-             setMessage("Please enter a city");
-            setTimeout(() => {
-             setMessage("");
-              }, 5000);
-               return;
-          }
-         setLoading(true);
-        const q = query(collection(db, "hospitals"), where("city", "==", city), orderBy("name", "asc"));
+    const fetchMoreHospitals = async () => {
+        //Get the last visible document
+        const lastVisible = hospitals[hospitals.length - 1];
+        const q = query(collection(db, "hospitals"), where("city", "==", city), orderBy("name", "asc"), startAfter(lastVisible), limit(perPage));
         try {
             const querySnapshot = await getDocs(q);
             const hospitals = querySnapshot.docs.map((doc) => ({
@@ -54,7 +71,40 @@ function useFetchHospitals() {
             }))
             setHospitals((prev) => [...prev, ...hospitals] as Hospital[]);
             setLoading(false);
-            //If the city entered is not in the database
+        } catch (error: any) {
+            setMessage(error.message);
+        }
+    };
+
+    const fetchPreviousHospitals = async () => {
+        //Get the first visible document
+        const firstVisible = hospitals[0];
+        const q = query(collection(db, "hospitals"), where("city", "==", city), orderBy("name", "asc"), endBefore(firstVisible), limitToLast(perPage));
+        try {
+            const querySnapshot = await getDocs(q);
+            const hospitals = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }))
+            setHospitals((prev) => [...prev, ...hospitals] as Hospital[]);
+            setLoading(false);
+        } catch (error: any) {
+            setMessage(error.message);
+        }
+    }
+
+
+    //handle Search Hospital
+    const handleSearchHospital = async () => {
+        const q = query(collection(db, "hospitals"), where("city", "==", city), orderBy("name", "asc"), limit(perPage));
+        try {
+            const querySnapshot = await getDocs(q);
+            const hospitals = querySnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }))
+            setHospitals(hospitals as Hospital[]);
+            setLoading(false);
             if (hospitals.length === 0) {
                 setMessage(`No hospital data found for ${city}`);
                 setTimeout(() => {
@@ -65,48 +115,9 @@ function useFetchHospitals() {
         } catch (error: any) {
             setMessage(error.message);
         }
-        //
-    };
+    }
 
-
-    const fetchMoreHospitals = async () => {
-        //Get the last visible document
-        const lastVisible = ("name");
-        
-        const q = query(collection(db, "hospitals"), orderBy("name", "asc"), startAfter(lastVisible), limit(perPage));
-        try {
-            const querySnapshot = await getDocs(q);
-            const hospitals = querySnapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }))
-            setHospitals((prev) => [...prev, ...hospitals] as Hospital[]);
-            setLoading(false);
-        } catch (error: any) {
-            setMessage(error.message);
-        }
-      console.log(startAfter("name"));
-        
-    };
-
-    const fetchPreviousHospitals = async () => {
-        //Get the first visible document
-        // const firstVisible = hospitals[0];
-        
-        const q = query(collection(db, "hospitals"), orderBy("name", "asc"), endBefore("createdAt"), limitToLast(perPage));
-        try {
-            const querySnapshot = await getDocs(q);
-            const hospitals = querySnapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }))
-            setHospitals((prev) => [...prev, ...hospitals] as Hospital[]);
-            setLoading(false);
-        } catch (error: any) {
-            setMessage(error.message);
-        }
-    };
-
+    
     //handle Search Hospital
    
 
@@ -115,3 +126,8 @@ function useFetchHospitals() {
 }
 
 export default useFetchHospitals;
+
+
+
+
+ 
