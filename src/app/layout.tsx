119829ef7791684d779b3a3b99bb685d5a944633
Adezb <temitopeceo@gmail.com>
2023-07-18 14:01:14 +0100

    meta update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,23 @@
 import "./globals.css";
 import { RecoilRoot } from "recoil";
 import React from "react";
-import Head from "next/head";
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <RecoilRoot>
-      <Head>
-        <title>MedCare Finder</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Find hospitals around you with ease"
-        />
-      </Head>
-
       <html lang="en">
+        <head>
+          <title>MedCare Finder</title>
+          <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta
+            name="description"
+            content="Find hospitals around you with ease"
+          />
+        </head>
         <body>{children}</body>
       </html>
     </RecoilRoot>
